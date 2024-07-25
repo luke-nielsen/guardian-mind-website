f47730ca16b27d30c560390c1553d88bbef4d505
luke-nielsen <116595556+luke-nielsen@users.noreply.github.com>
2024-07-25 12:09:40 -0600

    Add files via upload
    Initial files for website

---
+++ guardian-mind/script.js
@@ -0,0 +1,63 @@
+const globalContactMessage =document.getElementById('contact-message');
+
+function learnMore() {
+    alert("Learn more about how we can help you protect your kids online.");
+}
+
+function joinMailingList() {
+    const contact = document.getElementById('contact');
+    const contactMessage =document.getElementById('contact-message');
+    contact.scrollIntoView({ behavior: 'smooth' });
+    contactMessage.placeholder = 'I want to join the mailing list!'
+}
+
+function navJoinMailingList(event) {
+    event.preventDefault();
+    const contact = document.getElementById('contact');
+    const contactMessage =document.getElementById('contact-message');
+    contact.scrollIntoView({ behavior: 'smooth' });
+    contactMessage.placeholder = 'I want to join the mailing list!'
+}
+
+function goToDonatePage(event) {
+    event.preventDefault();
+    const donatePage = 'donate.html'
+    window.location.href = donatePage;
+}
+
+function goToHomePage(event) {
+    event.preventDefault();
+    const homePage = 'index.html'
+    window.location.href = homePage;
+}
+
+function donateJoinMailingList(event) {
+    event.preventDefault();
+    const mailingListPage = 'index.html#contact'
+    window.location.href = mailingListPage;
+    window.onload = function() {
+        const contactMessage =document.getElementById('contact-message');
+        contactMessage.placeholder = 'I want to join the mailing list!'
+    }
+}
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Thank you for your message!");
+});
+
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    const navList = document.getElementById('nav-links');
+    navList.classList.toggle('active');
+});
+
+document.getElementById('nav-links').addEventListener('click', function() {
+    const navList = document.getElementById('nav-links');
+    navList.classList.toggle('active')
+});
+
+document.getElementById('donate-logo').addEventListener('click', goToHomePage())
+
+// document.getElementById('contact-nav-link').addEventListener('click', joinMailingList())
+
+// document.getElementById('donate-nav-link').addEventListener('click', goToDonatePage())
