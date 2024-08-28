f26b9ebb1a0949027550fbbc438691f7d1d62985
luke-nielsen <116595556+luke-nielsen@users.noreply.github.com>
2024-08-28 15:38:17 -0600

    Delete admin directory

--- admin/login.js
+++
@@ -1,6 +0,0 @@
-const pwHolder = document.getElementById("password");
-document.getElementById("submitbtn").addEventListener("click", function() {
-    if(pwHolder.value === "Password"){
-        window.location.href = "menu.html";
-    }
-});
